493bac93c32b6dd78715f5c2d460694a1c446357
zhangxinxu <zhangxinxu@zhangxinxu.com>
2015-01-07 17:30:58 +0800

    make plugin of ppt more powerful and useable
    make plugin of ppt more powerful and useable

--- plugins/ppt/mobilebone.ppt.js
+++ plugins/ppt/mobilebone.ppt.js
@@ -5,32 +5,31 @@ if (window.Mobilebone && Mobilebone.support) {
 			index_page = 0, 
 			hash = location.hash.replace("#&", "");
 			
-		if (hash != "" && hash != "#") [].slice.call(pages).forEach(function(page, index) {
-			if (page.id == hash) {
+		[].slice.call(pages).forEach(function(page, index) {
+			if (!page.id) page.id = "page" + (index+1);
+			
+			if (hash != "" && hash != "#" && page.id == hash) {
 				index_page = index;
 			}
 		});
 		
 		var prev = function() {
-			if (index_page > 0) {
+			if (index_page > 0 && Mobilebone.transition(pages[index_page-1], pages[index_page], true) !== false) {
 				index_page--;
-				Mobilebone.transition(pages[index_page], pages[index_page + 1], true);
 			}
 		}, next = function() {
-			if (index_page < length_page - 1) {
+			if (index_page < length_page - 1 && Mobilebone.transition(pages[index_page + 1], pages[index_page]) !== false) {
 				index_page++;
-				Mobilebone.transition(pages[index_page], pages[index_page - 1]);
 			}	
 		};
 		
-		document.addEventListener("click", function(event) {
+		/*document.addEventListener("click", function(event) {
 			var target = event.target;
 			if (target.tagName.toLowerCase() == "a" || target.getParentElementByTag("a")) return;
 			next();
-		});
+		});*/
 		
 		document.addEventListener("keyup", function(event) {
-			console.log(event.keyCode);
 			switch (event.keyCode) {
 				case 38: case 37: {
 					prev();
@@ -57,5 +56,29 @@ if (window.Mobilebone && Mobilebone.support) {
 				prev();
 			}
 		});
+		
+		
+		Mobilebone.preventdefault = function(pagein, pageout) {
+			if (pageout == null) return;
+			var isBack = Mobilebone.isBack(pagein, pageout);
+			if (isBack == true) {
+				var elein = pageout.querySelector(".in");
+				if (elein) {
+					elein.style.display = "block";
+					elein.classList.remove("in");
+					elein.classList.add("out");
+					return true;
+				}
+			} else {
+				var eleout = pageout.querySelector(".out");
+				
+				if (eleout) {
+					eleout.classList.remove("out");
+					eleout.classList.add("in");
+					return true;
+				}
+			}
+		};
+		
 	})(window, document);
 }
