4a19a388fd06950d03f73efdaa9e29cc8dd14f1e
zhangxinxu <zhangxinxu@zhangxinxu.com>
2015-01-11 14:52:40 +0800

    update documents
    1. many APIs more info
    2. add data-container/data-classpage

--- docs/docs.js
+++ docs/docs.js
@@ -6,5 +6,12 @@
 	
 	getVersion = function(pagein) {
 		pagein.querySelector(".version").innerHTML = "v" + Mobilebone.VERSION;
-	};	
+	};
+	
+	tabButtonActive = function(pagein) {
+		var target = document.querySelectorAll(".tabview a")[pagein.id.replace(/\D/g, "") - 1];
+		var eleAcive = document.querySelector(".tabview .active");
+		if (eleAcive) eleAcive.classList.remove("active");
+		if (target) target.classList.add("active");
+	};
 })();
